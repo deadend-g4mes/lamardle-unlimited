b84d39bc4e32c9bb65d4138504782b188787575c
deadend-g4mes <leighton.h.carpenter@gmail.com>
2026-07-10 12:29:56 -0500

    update for discord activity integration

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 
 // 47a4f79b-9bda-4259-8170-a0f390ee7443
 
+import { DiscordSDK } from "@discord/embedded-app-sdk";
+const discordSdk = new DiscordSDK({ clientId: "1465072599385112578" });
+
 var result = { "title": "DJ Khaled" }
 let mysteryNumber = {}
 let mysterySong = {}
